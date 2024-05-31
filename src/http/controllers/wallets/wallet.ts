bb45c73677c9d043cb840803458ace2aadf87154
David-jos3 <davidgomesgc230@gmail.com>
2024-05-31 12:23:03 -0300

    feat: Add controller for wallet creation

---
+++ src/http/controllers/wallets/wallet.ts
@@ -0,0 +1,28 @@
+import { PrismaWalletRepository } from '@/http/repositories/prisma/prisma-wallet-repository';
+import { WalletUseCase } from '@/use-case/wallet';
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod'
+
+
+
+export async function wallet (request:FastifyRequest, reply: FastifyReply) {
+
+const createWalletSchema = z.object({
+ userId: z.string(),
+ balance: z.number()
+})
+
+const {userId, balance} = createWalletSchema.parse(request.body)
+  try{
+    const walletRepository = new PrismaWalletRepository()
+    const walletUseCase = new WalletUseCase(walletRepository)
+   const wallet = await walletUseCase.execute({
+      userId,
+      balance
+    })
+  } catch(error) {
+    if(error instanceof z.ZodError){
+      return reply.status(400).send({message: error.message})
+    }
+  }
+}
