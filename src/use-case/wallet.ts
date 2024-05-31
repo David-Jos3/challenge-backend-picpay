c5a5b762599f73fefcafc004485ee0109399bfb0
David-jos3 <davidgomesgc230@gmail.com>
2024-05-31 11:51:38 -0300

    feat: Add WalletUseCase for wallet creation functionality

---
+++ src/use-case/wallet.ts
@@ -0,0 +1,27 @@
+import { WalletRepository } from "@/http/repositories/wallet-repositories";
+import { Wallet } from "@prisma/client";
+import { WalletCreationError } from "./errors/wallet-creation-error";
+
+
+export interface WalletUseCaseRequest {
+  userId: string
+  balance: number
+}
+
+export interface WalletUseCaseResponse {
+  wallet: Wallet;
+}
+
+export class WalletUseCase {
+  constructor(private walletRepository: WalletRepository) {}
+
+  async execute({userId, balance }: WalletUseCaseRequest): Promise<WalletUseCaseResponse> {
+    const wallet = await this.walletRepository.create(userId, balance);
+
+    if (!wallet) {
+      throw new WalletCreationError();
+    }
+
+    return { wallet };
+  }
+}
