e54133aec0d940c1e7dec5c1a53bc3a1fc4f42a1
David-jos3 <davidgomesgc230@gmail.com>
2024-06-03 23:46:55 -0300

    feat: add a new route for transactions

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import fastify from "fastify";
 import { registerRoutes } from "./http/controllers/users/routes";
 import { walletRoutes } from "./http/controllers/wallets/routes";
+import { transationRoutes } from "./http/controllers/transations/router";
 
 export const app = fastify();
 
 app.register(registerRoutes)
 app.register(walletRoutes)
+app.register(transationRoutes)
 
